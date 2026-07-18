88a31d9caf93f8d9684d8501257f8ba2abd5ee6d
illiasemenov51-ui <illiasemenov51@gmail.com>
2026-07-18 14:00:00 +0200

    refactor: update application entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider, CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
 import { GamificationProvider } from "./context/GamificationContext";
-import { theme } from "./theme";
+import { QuickActionsProvider } from "./context/QuickActionsContext";
+import { AppThemeProvider } from "./theming/AppThemeProvider";
 import App from "./App";
 
 const queryClient = new QueryClient({
@@ -18,15 +18,16 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <AppThemeProvider>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <GamificationProvider>
-            <App />
+            <QuickActionsProvider>
+              <App />
+            </QuickActionsProvider>
           </GamificationProvider>
         </AuthProvider>
       </QueryClientProvider>
-    </ThemeProvider>
+    </AppThemeProvider>
   </StrictMode>
 );
